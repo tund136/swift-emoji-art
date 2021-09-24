3dab3ac15b6994d63ed7763ad7cac5a1ade1478d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-24 21:47:45 +0700

    Add Palette Control Button

--- EmojiArt/View/PaletteChooser.swift
+++ EmojiArt/View/PaletteChooser.swift
@@ -13,8 +13,28 @@ struct PaletteChooser: View {
     
     @EnvironmentObject var store: PaletteStore
     
+    @State private var chosenPaletteIndex = 0
+    
     var body: some View {
-        let palette = store.palette(at: 0)
+        let palette = store.palette(at: chosenPaletteIndex)
+        
+        HStack {
+            paletteControlButton
+            body(for: palette)
+        }
+        
+    }
+    
+    var paletteControlButton: some View {
+        Button {
+            chosenPaletteIndex = (chosenPaletteIndex + 1) % store.palettes.count
+        } label: {
+            Image(systemName: "paintpalette")
+        }
+        .font(emojiFont)
+    }
+    
+    func body(for palette: Palette) -> some View {
         HStack {
             Text(palette.name)
             ScrollingEmojisView(emojis: palette.emojis)
