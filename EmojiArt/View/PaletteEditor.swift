92e889ab4bb4c9ed3476affdfaf5d3dd794c02b7
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-24 22:19:45 +0700

    Add PaletteEditor View

---
+++ EmojiArt/View/PaletteEditor.swift
@@ -0,0 +1,20 @@
+//
+//  PaletteEditor.swift
+//  EmojiArt
+//
+//  Created by Danh Tu on 24/09/2021.
+//
+
+import SwiftUI
+
+struct PaletteEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteEditor()
+    }
+}
