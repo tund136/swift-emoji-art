2ec3f4d8c94c380072a2477b134346d252b1938e
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-24 22:04:50 +0700

    Create Context Menu for Palette Control Button with New and Delete

--- EmojiArt/View/PaletteChooser.swift
+++ EmojiArt/View/PaletteChooser.swift
@@ -34,6 +34,20 @@ struct PaletteChooser: View {
             Image(systemName: "paintpalette")
         }
         .font(emojiFont)
+        .contextMenu { contextMenu }
+    }
+    
+    @ViewBuilder
+    var contextMenu: some View {
+        AnimatedActionButton(title: "New", systemImage: "plus") {
+            store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
+        }
+        
+        AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
+            chosenPaletteIndex = store.removePalette(at: chosenPaletteIndex)
+        }
+        
+        
     }
     
     func body(for palette: Palette) -> some View {
