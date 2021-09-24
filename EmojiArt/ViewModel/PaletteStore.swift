58417f4588e7067051c0056fcbd684dd5a0de9de
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-24 16:04:17 +0700

    Create functions and insert palette in PaletteStore

--- EmojiArt/ViewModel/PaletteStore.swift
+++ EmojiArt/ViewModel/PaletteStore.swift
@@ -7,4 +7,52 @@
 
 import SwiftUI
 
+struct Palette: Identifiable {
+    var id: Int
+    var name: String
+    var emojis: String
+    
+    fileprivate init(id: Int, name: String, emojis: String) {
+        self.id = id
+        self.name = name
+        self.emojis = emojis
+    }
+}
 
+class PaletteStore: ObservableObject {
+    let name: String
+    
+    @Published var palettes = [Palette]()
+    
+    init(named name: String) {
+        self.name = name
+        if palettes.isEmpty {
+            insertPalette(named: "Vehicles", emojis: "🚗🚙🚕🚌🚎🏎🚓🚑🚒🚐🛻🚚🚛🚜🛴🚲🛵🏍🛺🚍🚘🚖🚔✈️🚢🚤🛥")
+            insertPalette(named: "Sports", emojis: "⚽️🏀🏈⚾️🥎🎾🏐🏉🥏🎱🪀🏸🏒🏑🥍🏏🪃🥅⛳️🪁🏹🎣🤿🥊🥋🛹🛼🛷⛸🥌🎿⛷🏂🪂")
+            insertPalette(named: "Animals", emojis: "🐶🐱🐭🐹🐰🦊🐻🐼🐻‍❄️🐨🐯🦁🐮🐷🐽🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝")
+        }
+    }
+    
+    // MARK: -Intent(s)
+    
+    func palette(at index: Int) -> Palette {
+        let safeIndex = min(max(index, 0), palettes.count - 1)
+        return palettes[safeIndex]
+    }
+    
+    @discardableResult // Ignore return values
+    func removePalette(at index: Int) -> Int {
+        if palettes.count > 1, palettes.indices.contains(index) {
+            palettes.remove(at: index)
+        }
+        
+        return index % palettes.count
+    }
+    
+    func insertPalette(named name: String, emojis: String? = nil, at index: Int = 0) {
+        let unique = (palettes.max(by: { $0.id < $1.id })?.id ?? 0) + 1
+        let palette = Palette(id: unique, name: name, emojis: emojis ?? "")
+        let safeIndex = min(max(index, 0), palettes.count)
+        palettes.insert(palette, at: safeIndex)
+    }
+}
