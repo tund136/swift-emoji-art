ca57119466de57ea12325ed4c7b5545e2c199440
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-25 08:29:53 +0700

    Using Palette Optional for editing status

--- EmojiArt/View/PaletteChooser.swift
+++ EmojiArt/View/PaletteChooser.swift
@@ -40,12 +40,14 @@ struct PaletteChooser: View {
     @ViewBuilder
     var contextMenu: some View {
         AnimatedActionButton(title: "Edit", systemImage: "pencil") {
-            editing = true
+            //            editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
         }
         
         AnimatedActionButton(title: "New", systemImage: "plus") {
             store.insertPalette(named: "New", emojis: "", at: chosenPaletteIndex)
-            editing = true
+            //            editing = true
+            paletteToEdit = store.palette(at: chosenPaletteIndex)
         }
         
         AnimatedActionButton(title: "Delete", systemImage: "minus.circle") {
@@ -76,12 +78,16 @@ struct PaletteChooser: View {
         }
         .id(palette.id)
         .transition(rollTransition)
-        .popover(isPresented: $editing) {
-            PaletteEditor(palette: $store.palettes[chosenPaletteIndex])
+        //        .popover(isPresented: $editing) {
+        //            PaletteEditor(palette: $store.palettes[chosenPaletteIndex])
+        //        }
+        .popover(item: $paletteToEdit) { palette in
+            PaletteEditor(palette: $store.palettes[palette])
         }
     }
     
-    @State private var editing = false
+    //    @State private var editing = false
+    @State private var paletteToEdit: Palette?
     
     var rollTransition: AnyTransition {
         AnyTransition.asymmetric(insertion: .offset(x: 0, y: emojiFontSize), removal: .offset(x: 0, y: -emojiFontSize))
--- EmojiArt/ViewModel/PaletteStore.swift
+++ EmojiArt/ViewModel/PaletteStore.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct Palette: Identifiable, Codable {
+struct Palette: Identifiable, Codable, Hashable {
     var id: Int
     var name: String
     var emojis: String
