c508b83a466eab32dbf0d0749ab09c1c807a7f0d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-25 08:48:42 +0700

    Add nameSection and addEmojisSection to PaletteEditor

--- EmojiArt/View/PaletteEditor.swift
+++ EmojiArt/View/PaletteEditor.swift
@@ -13,9 +13,34 @@ struct PaletteEditor: View {
     @Binding var palette: Palette
     var body: some View {
         Form {
-            TextField("Name", text: $palette.name)
+            nameSection
+            addEmojisSection
         }
         .frame(minWidth: 300, minHeight: 350)
+    }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $palette.name)
+        }
+    }
+    
+    @State private var emojisToAdd = ""
+    
+    var addEmojisSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+        }
     }
 }
 
