bd20039b5ea68f8619b95af8f49248ae2ac3db40
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-25 08:57:34 +0700

    Add PaletteManager to manage emojis

---
+++ EmojiArt/View/PaletteManager.swift
@@ -0,0 +1,20 @@
+//
+//  PaletteManager.swift
+//  EmojiArt
+//
+//  Created by Danh Tu on 25/09/2021.
+//
+
+import SwiftUI
+
+struct PaletteManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaletteManager_Previews: PreviewProvider {
+    static var previews: some View {
+        PaletteManager()
+    }
+}
