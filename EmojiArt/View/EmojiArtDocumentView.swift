1143256397a68b9c79a86175fa4449d271055c42
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-26 20:48:59 +0700

    Add UndoButton on toolbar

--- EmojiArt/View/EmojiArtDocumentView.swift
+++ EmojiArt/View/EmojiArtDocumentView.swift
@@ -76,6 +76,12 @@ struct EmojiArtDocumentView: View {
                     zoomToFit(image, in: geometry.size)
                 }
             }
+            .toolbar {
+                UndoButton(
+                    undo: undoManager?.optionalUndoMenuItemTitle,
+                    redo: undoManager?.optionalRedoMenuItemTitle
+                )
+            }
         }
     }
     
