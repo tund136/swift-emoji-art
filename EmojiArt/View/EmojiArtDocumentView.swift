d1206b53078925b8f287595c539a35019f591924
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-27 08:51:32 +0700

    AnimatedActionButton: undo and redo

--- EmojiArt/View/EmojiArtDocumentView.swift
+++ EmojiArt/View/EmojiArtDocumentView.swift
@@ -78,12 +78,20 @@ struct EmojiArtDocumentView: View {
             }
             .toolbar {
                 ToolbarItemGroup(placement: .bottomBar) {
-                    UndoButton(
-                        undo: undoManager?.optionalUndoMenuItemTitle,
-                        redo: undoManager?.optionalRedoMenuItemTitle
-                    )
                     AnimatedActionButton(title: "Paste Background", systemImage: "doc.on.clipboard") {
                         pasteBackground()
+                    }
+                    if let undoManager = undoManager {
+                        if undoManager.canUndo {
+                            AnimatedActionButton(title: undoManager.undoActionName, systemImage: "arrow.uturn.backward") {
+                                undoManager.undo()
+                            }
+                        }
+                        if undoManager.canRedo {
+                            AnimatedActionButton(title: undoManager.redoActionName, systemImage: "arrow.uturn.forward") {
+                                undoManager.redo()
+                            }
+                        }
                     }
                 }
             }
