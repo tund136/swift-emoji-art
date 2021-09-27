5c7de0acf67adff9886f3d765323e598cd30eb48
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-27 09:20:41 +0700

    PasteBackground works

--- EmojiArt/View/EmojiArtDocumentView.swift
+++ EmojiArt/View/EmojiArtDocumentView.swift
@@ -99,7 +99,16 @@ struct EmojiArtDocumentView: View {
     }
     
     private func pasteBackground() {
-        
+        if let imageData = UIPasteboard.general.image?.jpegData(compressionQuality: 1.0) {
+            document.setBackground(.imageData(imageData), undoManager: undoManager)
+        } else if let url = UIPasteboard.general.url?.imageURL {
+            document.setBackground(.url(url), undoManager: undoManager)
+        } else {
+            alertToShow = IdentifiableAlert (
+                title: "Past Background",
+                message: "There is no image currently on the paste board."
+            )
+        }
     }
     
     @State private var autoZoom = false
